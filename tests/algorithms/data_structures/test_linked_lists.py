715dc0832199508d27682496e9ca184e7f766cf8
Mikołaj Warda <mikolaj.warda@gmail.com>
2026-01-28 10:56:55 +0100

    fix: Changed `LinkedList` to `SingleLinkedList`

--- tests/algorithms/data_structures/test_linked_lists.py
+++ tests/algorithms/data_structures/test_linked_lists.py
@@ -1,5 +1,5 @@
 import pytest
-from algorithms.data_structures.linked_lists import LinkedList, Node
+from algorithms.data_structures.linked_lists import SingleLinkedList, Node
 
 
 class TestNode:
@@ -15,16 +15,16 @@ class TestNode:
         assert repr(node) == "Node(value=10)"
 
 
-class TestLinkedList:
-    """Tests for LinkedList class"""
+class TestSingleLinkedList:
+    """Tests for SingleLinkedList class"""
     
     def test_empty_list_initialization(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         assert ll.head is None
         assert ll.tail is None
     
     def test_append_to_empty_list(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         assert ll.head is not None
         assert ll.tail is not None
@@ -33,7 +33,7 @@ class TestLinkedList:
         assert ll.head == ll.tail
     
     def test_append_multiple_elements(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -51,7 +51,7 @@ class TestLinkedList:
         assert values == [1, 2, 3]
     
     def test_prepend_to_empty_list(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.prepend(1)
         assert ll.head is not None
         assert ll.tail is not None
@@ -60,7 +60,7 @@ class TestLinkedList:
         assert ll.head == ll.tail
     
     def test_prepend_multiple_elements(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.prepend(1)
         ll.prepend(2)
         ll.prepend(3)
@@ -78,7 +78,7 @@ class TestLinkedList:
         assert values == [3, 2, 1]
     
     def test_mixed_append_and_prepend(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(2)
         ll.prepend(1)
         ll.append(3)
@@ -93,14 +93,14 @@ class TestLinkedList:
         assert values == [0, 1, 2, 3]
     
     def test_pop_head_from_empty_list(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         result = ll.pop_head()
         assert result is None
         assert ll.head is None
         assert ll.tail is None
     
     def test_pop_head_from_single_element_list(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         result = ll.pop_head()
         
@@ -109,7 +109,7 @@ class TestLinkedList:
         assert ll.tail is None
     
     def test_pop_head_multiple_times(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -129,7 +129,7 @@ class TestLinkedList:
         assert ll.pop_head() is None
     
     def test_pop_head_updates_tail_when_empty(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.pop_head()
         
@@ -137,7 +137,7 @@ class TestLinkedList:
         assert ll.tail is None
     
     def test_append_after_popping_all(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.pop_head()
         ll.append(2)
@@ -146,7 +146,7 @@ class TestLinkedList:
         assert ll.tail.value == 2
     
     def test_different_data_types(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append("hello")
         ll.append(42)
         ll.append(3.14)
@@ -159,14 +159,14 @@ class TestLinkedList:
         assert ll.pop_head() is None  # This is from empty list
     
     def test_delete_from_empty_list(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         result = ll.delete(0)
         assert result is None
         assert ll.head is None
         assert ll.tail is None
     
     def test_delete_at_index_0_single_element(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         result = ll.delete(0)
         
@@ -175,7 +175,7 @@ class TestLinkedList:
         assert ll.tail is None
     
     def test_delete_at_index_0_multiple_elements(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -186,7 +186,7 @@ class TestLinkedList:
         assert ll.tail.value == 3
     
     def test_delete_at_last_index(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -205,7 +205,7 @@ class TestLinkedList:
         assert values == [1, 2]
     
     def test_delete_middle_element(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -225,7 +225,7 @@ class TestLinkedList:
         assert ll.tail.value == 4
     
     def test_delete_multiple_elements_sequentially(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         for i in range(5):
             ll.append(i)
         
@@ -257,7 +257,7 @@ class TestLinkedList:
         assert values == [1, 3]
     
     def test_delete_negative_index(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         
@@ -273,7 +273,7 @@ class TestLinkedList:
         assert values == [1, 2]
     
     def test_delete_out_of_bounds_index(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         
@@ -289,7 +289,7 @@ class TestLinkedList:
         assert values == [1, 2]
     
     def test_delete_all_elements_by_index(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.append(3)
@@ -304,7 +304,7 @@ class TestLinkedList:
         assert ll.delete(0) is None
     
     def test_delete_then_append(self):
-        ll = LinkedList()
+        ll = SingleLinkedList()
         ll.append(1)
         ll.append(2)
         ll.delete(0)
@@ -317,6 +317,112 @@ class TestLinkedList:
             current = current.next
         assert values == [2, 3]
         assert ll.head.value == 2
+        assert ll.tail.value == 3
+    
+    def test_pop_tail_from_empty_list(self):
+        ll = SingleLinkedList()
+        result = ll.pop_tail()
+        assert result is None
+        assert ll.head is None
+        assert ll.tail is None
+    
+    def test_pop_tail_from_single_element_list(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        result = ll.pop_tail()
+        
+        assert result == 1
+        assert ll.head is None
+        assert ll.tail is None
+    
+    def test_pop_tail_from_two_element_list(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        ll.append(2)
+        result = ll.pop_tail()
+        
+        assert result == 2
+        assert ll.head.value == 1
+        assert ll.tail.value == 1
+        assert ll.head == ll.tail
+    
+    def test_pop_tail_multiple_times(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        ll.append(2)
+        ll.append(3)
+        
+        assert ll.pop_tail() == 3
+        assert ll.head.value == 1
+        assert ll.tail.value == 2
+        
+        assert ll.pop_tail() == 2
+        assert ll.head.value == 1
+        assert ll.tail.value == 1
+        
+        assert ll.pop_tail() == 1
+        assert ll.head is None
+        assert ll.tail is None
+        
+        assert ll.pop_tail() is None
+    
+    def test_pop_tail_updates_head_when_empty(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        ll.pop_tail()
+        
+        assert ll.head is None
+        assert ll.tail is None
+    
+    def test_pop_tail_large_list(self):
+        ll = SingleLinkedList()
+        for i in range(1, 6):
+            ll.append(i)
+        
+        # List: [1, 2, 3, 4, 5]
+        assert ll.pop_tail() == 5
+        assert ll.tail.value == 4
+        
+        assert ll.pop_tail() == 4
+        assert ll.tail.value == 3
+        
+        # Verify remaining list
+        values = []
+        current = ll.head
+        while current:
+            values.append(current.value)
+            current = current.next
+        assert values == [1, 2, 3]
+    
+    def test_mixed_pop_head_and_pop_tail(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        ll.append(2)
+        ll.append(3)
+        ll.append(4)
+        
+        assert ll.pop_head() == 1  # [2, 3, 4]
+        assert ll.pop_tail() == 4  # [2, 3]
+        assert ll.pop_head() == 2  # [3]
+        assert ll.pop_tail() == 3  # []
+        
+        assert ll.head is None
+        assert ll.tail is None
+    
+    def test_append_after_popping_tail(self):
+        ll = SingleLinkedList()
+        ll.append(1)
+        ll.append(2)
+        ll.pop_tail()
+        ll.append(3)
+        
+        values = []
+        current = ll.head
+        while current:
+            values.append(current.value)
+            current = current.next
+        assert values == [1, 3]
+        assert ll.head.value == 1
         assert ll.tail.value == 3
 
 
